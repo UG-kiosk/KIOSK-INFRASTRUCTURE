143ca50e3799036dac7c80d4750daa6631a9dedb
Jakub Zapiorkowski <zapiorkowski.j@gmail.com>
2024-05-06 17:31:12 +0200

    add port to scrapers api

--- develop/scrapers-kiosk/main.tf
+++ develop/scrapers-kiosk/main.tf
@@ -12,33 +12,35 @@ data "terraform_remote_state" "ug_kiosk_api_gw_state" {
   }
 }
 
-
 locals {
   docker_image = "${module.shared_envs_dev.docker_username}/kiosk-scrapers-dev:${var.docker_image_tag}"
 }
 
 resource "azurerm_service_plan" "scrapers-kiosk" {
-    name                = var.app_service_name
-    resource_group_name = module.shared_envs_dev.resource_group_name
-    location            = module.shared_envs_dev.location
-    os_type             = "Linux"
-    sku_name            = "F1"
+  name                = var.app_service_name
+  resource_group_name = module.shared_envs_dev.resource_group_name
+  location            = module.shared_envs_dev.location
+  os_type             = "Linux"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "scrapers-kiosk" {
-    name                = var.app_service_name
-    resource_group_name = azurerm_service_plan.scrapers-kiosk.resource_group_name
-    location            = azurerm_service_plan.scrapers-kiosk.location
-    service_plan_id     = azurerm_service_plan.scrapers-kiosk.id
-    
+  name                = var.app_service_name
+  resource_group_name = azurerm_service_plan.scrapers-kiosk.resource_group_name
+  location            = azurerm_service_plan.scrapers-kiosk.location
+  service_plan_id     = azurerm_service_plan.scrapers-kiosk.id
+  
+  site_config {
+    always_on = false
+    api_management_api_id = data.terraform_remote_state.ug_kiosk_api_gw_state.outputs.api_management_api_id
 
-    site_config {
-        always_on = false
-        api_management_api_id = data.terraform_remote_state.ug_kiosk_api_gw_state.outputs.api_management_api_id
-
-        application_stack {
-            docker_image_name   = local.docker_image
-            docker_registry_url = module.shared_envs_dev.docker_registry
-      }
+    application_stack {
+      docker_image_name   = local.docker_image
+      docker_registry_url = module.shared_envs_dev.docker_registry
     }
+  }
+
+  app_settings = {
+    WEBSITES_PORT = "3000"
+  }
 }
